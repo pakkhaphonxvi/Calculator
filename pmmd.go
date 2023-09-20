f7527a136c7dcd7b3d676e2655a15be49376b930
pakkhaphonxvi <pakkhaphon.dev@gmail.com>
2023-09-21 01:59:30 +0700

    add func

--- pmmd.go
+++ pmmd.go
@@ -1,11 +1,17 @@
 package myCalculator
 
+import "fmt"
+
 func Plus(A int, B int) int{
-	x =: A+B
+	x =: A+B;
 	return x;
 }
 
 func Minus(A int, B int) int{
-	x =: A-B
+	x =: A-B;
 	return x;
+}
+
+func Out(m string) {
+	fmt.Printf("text: %s", m);
 }
