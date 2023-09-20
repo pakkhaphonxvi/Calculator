dd5d92449c7d10564cb54ab42cab7ef1ad7a8fbf
pakkhaphonxvi <pakkhaphon.dev@gmail.com>
2023-09-21 02:05:36 +0700

    make it OUT alive

--- pmmd.go
+++ pmmd.go
@@ -2,15 +2,15 @@ package myCalculator
 
 import "fmt"
 
-// func Plus(A int, B int) int{
-// 	x =: A+B;
-// 	return x;
-// }
+func Plus(A int, B int) int{
+	x =: A+B;
+	return x;
+}
 
-// func Minus(A int, B int) int{
-// 	x =: A-B;
-// 	return x;
-// }
+func Minus(A int, B int) int{
+	x =: A-B;
+	return x;
+}
 
 func Out(m string) {
 	fmt.Printf("text: %s", m);
