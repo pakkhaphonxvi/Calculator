c713d19cea66081577f54ca1df8e93eadd3c2d3b
pakkhaphonxvi <pakkhaphon.dev@gmail.com>
2023-09-21 01:51:38 +0700

    edit function

--- pmmd.go
+++ pmmd.go
@@ -1,11 +1,11 @@
 package myCalculator
 
-func Plus(A int, B int) {
+func Plus(A int, B int) int{
 	x =: A+B
 	return x;
 }
 
-func Minus(A int, B int) {
+func Minus(A int, B int) int{
 	x =: A-B
 	return x;
 }
