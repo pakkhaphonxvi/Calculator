a908d01847bc593d19221947cb2a3d7e0f942e58
pakkhaphonxvi <pakkhaphon.dev@gmail.com>
2023-09-21 02:14:05 +0700

    change

--- pmmd.go
+++ pmmd.go
@@ -3,13 +3,13 @@ package myCalculator
 import "fmt"
 
 func Plus(A int, B int) int{
-	return A+B;
+	return A+B
 }
 
 func Minus(A int, B int) int{
-	return A-B;
+	return A-B
 }
 
 func Out(m string) {
-	fmt.Printf("text: %s", m);
+	fmt.Printf("text: %s", m)
 }
